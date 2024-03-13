95fbe2733c0f1e9b00772d518c6382a3aebfa65c
Nayema Ferdoushi <134846919+NAYEMA26@users.noreply.github.com>
2024-03-13 10:46:08 +0600

    Update allElements.cpp

--- headers/allElements.cpp
+++ headers/allElements.cpp
@@ -1,6 +1,9 @@
 #include<vector>
 #include "../elements/water.h"
 #include "../elements/Silver.h"
+#include "../elements/Diamond.h"
+#include "../elements/Suger.h"
+
 #include "baseElement.h"
 
 
@@ -9,5 +12,8 @@ vector<baseElement*> getAllElements()
     vector<baseElement*> elements;
     elements.push_back(new water());
     elements.push_back(new Silver());
+    elements.push_back(new Diamond());
+    elements.push_back(new Suger());
+    
     return elements;
 }
