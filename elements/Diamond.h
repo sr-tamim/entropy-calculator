97721b013e3c6f83b5bd5dbda62068c5de8df9ed
Nayema Ferdoushi <134846919+NAYEMA26@users.noreply.github.com>
2024-03-13 10:32:29 +0600

    Create Diamond.h

---
+++ elements/Diamond.h
@@ -0,0 +1,30 @@
+#ifndef Diamond_HPP_INCLUDED
+#define Diamond_HPP_INCLUDED
+
+#include "../headers/baseElement.h"
+
+class Diamond : public baseElement
+{
+public:
+    
+    Diamond()
+    {
+        // Diamond (Carbon)
+        
+        setElementName("Diamond");
+        
+        setSpecificHeatLiquid(0); // No liquid state
+        setSpecificHeatSolid(502); // J/kg.K
+        
+        // No specific heat capacity for gas
+        
+        setLatentHeatOfFusion(11700); // J/kg
+        setLatentHeatOfVaporization(0); // No vaporization
+        
+        setMeltingPoint(0); // Diamond sublimes at 3915 K
+        setBoilingPoint(0); // Diamond sublimes before boiling
+    }
+    ~Diamond();
+};
+
+#endif // DIAMOND_HPP_INCLUDED
