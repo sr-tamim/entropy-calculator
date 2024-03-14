969f25a5822c5a3cabaf72939f8bf96f9e2ad32c
titan000001 <tawhid.10a@gmail.com>
2024-03-14 13:14:43 +0600

    added Hydrogen and fixed some issues
    -connected Oxygen and Hydrogen to the allElements.cpp
    -fixed some errors

--- headers/allElements.cpp
+++ headers/allElements.cpp
@@ -1,6 +1,8 @@
 #include<vector>
 #include "../elements/water.h"
 #include "../elements/Silver.h"
+#include "../elements/Oxygen.h"
+#include "../elements/Hydrogen.h"
 #include "baseElement.h"
 
 
@@ -9,5 +11,8 @@ vector<baseElement*> getAllElements()
     vector<baseElement*> elements;
     elements.push_back(new water());
     elements.push_back(new Silver());
+    elements.push_back(new Oxygen());
+    elements.push_back(new Hydrogen());
+
     return elements;
 }
