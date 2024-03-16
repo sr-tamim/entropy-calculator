2d228ce9ca1f1e66bc96017b78b3b12ca1824992
SR Tamim <saifur.rahman.tamim1@gmail.com>
2024-03-16 13:24:17 +0600

    remove gmp.h dependency from cbp

--- headers/allElements.cpp
+++ headers/allElements.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <memory>
-#include "elements/water.h"
-#include "elements/Silver.h"
+#include "../elements/water.h"
+#include "../elements/Silver.h"
 #include "baseElement.h"
 
 vector<unique_ptr<baseElement>> getAllElements()
