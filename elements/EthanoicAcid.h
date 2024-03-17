6597b10bd19334e4217604d2db2bb1808ed53e38
Nayema Ferdoushi <134846919+NAYEMA26@users.noreply.github.com>
2024-03-17 23:55:26 +0600

    Create EthanoicAcid.h

---
+++ elements/EthanoicAcid.h
@@ -0,0 +1,28 @@
+#ifndef EthanoicAcid_HPP_INCLUDED
+#define EthanoicAcid_HPP_INCLUDED
+
+#include "../headers/baseElement.h"
+
+class EthanoicAcid : public baseElement
+{
+public:
+    EthanoicAcid()
+    {
+        setElementName("Ethanoic Acid");
+        
+        setSpecificHeatLiquid(2400); // J/kg.K
+        setSpecificHeatSolid(2400);//J/kg.K (Approximately)
+        setSpecificHeatGas(1630);//J/kg.K (Approximately)
+        
+        setLatentHeatOfFusion(120000); // J/kg (Approximately)
+        setLatentHeatOfVaporization(363000);  //J/kg (Approximately)
+        
+        setMeltingPoint(290); //K (Approximately)
+        setBoilingPoint(391); // K (Approximately)
+        
+    }
+    
+    ~EthanoicAcid();
+};
+
+#endif // EthanoicAcid_HPP_INCLUDED
